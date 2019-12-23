dd58c3cfc424d49f838ce7b30c05e5b7c18c0b21
maphnew <zcm3118@gmail.com>
2019-12-23 15:59:53 +0900

    login logout

--- app_session.js
+++ app_session.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const session = require('express-session')
 const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(session({
     secret: 'ASDM3JH12@R*HJ7436DUJ@IF',
     resave: false,
@@ -18,6 +20,62 @@ app.get('/count', (req,res)=> {
     }
 
     res.send('count: ' + req.session.count)
+})
+
+app.get('/auth/logout', (req,res) => {
+    delete req.session.displayName
+    res.redirect('/welcome')
+})
+
+app.get('/welcome', (req,res) => {
+    if (req.session.displayName) {
+        res.send(`
+            <h1>Hello, ${req.session.displayName}</h1>
+            <a href="/auth/logout">logout</a>
+        `)
+    } else {
+        res.send(`
+            <h1>Welcome</h1>
+            <a href="/auth/login">Login</a>
+        `)
+    }
+   
+})
+
+app.post('/auth/login', (req,res) => {
+    const user = {
+        username: 'user',
+        password: '1111',
+        displayName: 'KSC'
+    }
+
+    const uname = req.body.username
+    const pwd = req.body.password
+    if(uname == user.username && pwd === user.password){
+        req.session.displayName = user.displayName
+        console.log(req.session)
+        res.redirect('/welcome')
+    } else {
+        res.send('Who are you? <a href="/auth/login">login</a>')
+    }
+
+})
+
+app.get('/auth/login', (req,res) => {
+    const output = `
+    <form action="/auth/login" method="post">
+        <p>
+            <input type="text" name="username" placeholder="username">
+        </p>
+        <p>
+            <input type="password" name="password" placeholder="password">
+        </p>
+        <p>
+            <input type="submit">
+        </p>
+    </form>
+    `
+    res.send(output)
 })
 
 // app.get('/tmp', (req,res) =>{
